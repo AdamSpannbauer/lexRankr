05fce6e031d8981501f5e07cc9b794405e92af1c
Adam Spannbauer <SpannbauerAdam@gmail.com>
2017-01-13 14:47:33 -0500

    finished writing first round of tests for all functions

---
+++ tests/testthat/test-lexRank.R
@@ -0,0 +1,40 @@
+context("lexRank")
+
+# test object out str and class ---------------------------------------
+test_that("object out str and class", {
+  testDocs <- c("Testing 1, 2, 3.", 
+                "Is everything working as expected in my test?",
+                "Is it working?")
+  testResult <- lexRank(testDocs, Verbose = FALSE)
+  
+  expect_equal(class(testResult), "data.frame")
+  expect_equal(names(testResult), c("docId","sentenceId", "sentence","value"))
+  expect_true(is.character(testResult$sentenceId))
+  expect_true(is.character(testResult$sentence))
+  expect_true(is.numeric(testResult$value))
+})
+
+# test bad inputs ---------------------------------------
+test_that("bad inputs", {
+  expect_error(lexRank(FALSE, Verbose = FALSE))
+  expect_error(lexRank(NULL, Verbose = FALSE))
+})
+
+# test object out value
+test_that("object out value", {
+  testDocs <- c("Testing 1, 2, 3.", 
+                "Is everything working as expected in my test?",
+                "Is it working?")
+  
+  testResult <- lexRank(testDocs, Verbose = FALSE) %>% 
+    dplyr::mutate(value = round(value, 5))
+  
+  expectedResult <- data.frame(docId = c(2L, 1L, 3L),
+                               sentenceId = c("2_1", "1_1", "3_1"),
+                               sentence = c("Is everything working as expected in my test?", 
+                                            "Testing 1, 2, 3.", "Is it working?"),
+                               value = c(0.48649, 0.25676, 0.25676),
+                               stringsAsFactors = FALSE)
+  
+  expect_identical(testResult, expectedResult)
+})
---
+++ tests/testthat/test-lexRankFromSimil.R
@@ -0,0 +1,69 @@
+context("lexRankFromSimil")
+
+# test object out str and class ---------------------------------------
+test_that("object out str and class", {
+  testDocs <- c("Testing 1, 2, 3.", 
+                "Is everything working as expected in my test?",
+                "Is it working?")
+  tokenDf <- sentenceTokenParse(testDocs)$tokens
+  
+  similDf <- sentenceSimil(sentenceId = tokenDf$sentenceId,
+                           token = tokenDf$token,
+                           docId = tokenDf$docId)
+  
+  testResult <- lexRankFromSimil(similDf$sent1, similDf$sent2, similDf$similVal)
+  
+  expect_equal(class(testResult), "data.frame")
+  expect_equal(names(testResult), c("sentenceId", "value"))
+  expect_true(is.character(testResult$sentenceId))
+  expect_true(is.numeric(testResult$value))
+})
+
+# test bad inputs ---------------------------------------
+test_that("bad inputs", {
+  testDocs <- c("Testing 1, 2, 3.", 
+                "Is everything working as expected in my test?",
+                "Is it working?")
+  tokenDf <- sentenceTokenParse(testDocs)$tokens
+  
+  similDf <- sentenceSimil(sentenceId = tokenDf$sentenceId,
+                           token = tokenDf$token,
+                           docId = tokenDf$docId)
+  
+  expect_error(lexRankFromSimil(NULL, similDf$sent2, similDf$similVal))
+  expect_error(lexRankFromSimil(c(1,2), similDf$sent2, similDf$similVal))
+  expect_error(lexRankFromSimil(similDf$sent1, similDf$sent2, c("a","b","c")))
+  expect_error(lexRankFromSimil(similDf$sent1, similDf$sent2, similDf$similVal, threshold = NULL))
+  expect_error(lexRankFromSimil(similDf$sent1, similDf$sent2, similDf$similVal, damping = NULL))
+  
+})
+
+# test object out value
+test_that("object out value", {
+  testDocs <- c("Testing 1, 2, 3.", 
+                "Is everything working as expected in my test?",
+                "Is it working?")
+  tokenDf <- sentenceTokenParse(testDocs)$tokens
+  
+  similDf <- sentenceSimil(sentenceId = tokenDf$sentenceId,
+                           token = tokenDf$token,
+                           docId = tokenDf$docId)
+  
+  testResult <- lexRankFromSimil(similDf$sent1, similDf$sent2, similDf$similVal) %>% 
+    dplyr::mutate(value = round(value, 5))
+  
+  expectedResult <- data.frame(sentenceId = c("1_1", "2_1", "3_1"),
+                               value = c(0.25676, 0.48649, 0.25676),
+                               stringsAsFactors = FALSE)
+  
+  expect_identical(testResult, expectedResult)
+  
+  testResult <- lexRankFromSimil(similDf$sent1, similDf$sent2, similDf$similVal, continuous = TRUE) %>% 
+    dplyr::mutate(value = round(value, 5))
+  
+  expectedResult <- data.frame(sentenceId = c("1_1", "2_1", "3_1"),
+                               value = c(0.25676, 0.48649, 0.25676),
+                               stringsAsFactors = FALSE)
+  
+  expect_identical(testResult, expectedResult)
+})
